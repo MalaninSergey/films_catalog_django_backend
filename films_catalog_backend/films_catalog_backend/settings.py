4a0f08aeffd34d79e5be31d6ced267b5d3e614d2
Sergey Malanin <malanin.sergey.n@gmail.com>
2021-06-01 00:38:23 +0300

    Add postgres data connection

--- films_catalog_backend/films_catalog_backend/settings.py
+++ films_catalog_backend/films_catalog_backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'films_catalog_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'films_catalog',
+        'USER': 'admin',
+        'PASSWORD': os.environ.get('FILMS_CATALOG_DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
